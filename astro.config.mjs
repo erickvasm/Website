f889984c0f4cb754210d7c018abf54b8e69d1233
Erick Vasquez <erickmurillo0807@gmail.com>
2023-03-11 12:59:59 -0600

    feat: new 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,9 +14,5 @@ export default defineConfig({
   routes: [
     { path: "/", component: "src/pages/index.astro" },
     { path: "/404", component: "src/pages/no-found.astro" },
-    { path: "/es", component: "src/pages/index-es.astro" } 
-  ],
-  headers: {
-    "Content-Language": "es"
-  }
+  ]
 });
