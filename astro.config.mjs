ef35d5215a12b712c90970db2ca5dba685701058
Erick Vasquez <erickmurillo0807@gmail.com>
2024-01-12 11:45:29 -0600

    upgrade to astro 4.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
+//import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.erickvasm.com",
-  integrations: [tailwind(), image()],
-  vite: {
-    ssr: {
-      external: ["@11ty/eleventy-img", "svgo"]
-    }
-  },
+  integrations: [tailwind(), icon()],
   routes: [
     { path: "/", component: "src/pages/index.astro" },
     { path: "/404", component: "src/pages/no-found.astro" },
