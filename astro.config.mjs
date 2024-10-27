3399181f8b8b6fd15ea3303ba33a9d5c0e812365
Erick Vasquez <55751600+erickvasm@users.noreply.github.com>
2024-10-27 14:09:38 -0600

    Preview (#4)
    * feat: add blogs api
    
    * feat: add blogs api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
-//import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.erickvasm.com",
   integrations: [tailwind(), icon()],
   routes: [
     { path: "/", component: "src/pages/index.astro" },
+    { path: "/blog", component: "src/pages/blog.astro" },
     { path: "/404", component: "src/pages/no-found.astro" },
   ]
 });
---
+++ src/api/types/DEVArticle.ts
@@ -0,0 +1,36 @@
+export interface DEVArticle {
+  type_of: string;
+  id: number;
+  title: string;
+  description: string;
+  published: boolean;
+  published_at: string;
+  slug: string;
+  path: string;
+  url: string;
+  comments_count: number;
+  public_reactions_count: number;
+  page_views_count: number;
+  published_timestamp: string;
+  body_markdown: string;
+  positive_reactions_count: number;
+  cover_image: string | null;
+  tag_list: string[] | string;
+  canonical_url: string;
+  reading_time_minutes: number;
+  user: {
+    name: string;
+    username: string;
+    twitter_username: string | null;
+    github_username: string | null;
+    user_id: number;
+    website_url: string | null;
+    profile_image: string;
+    profile_image_90: string;
+  };
+}
+
+export interface GetArticlesOptions {
+  pages?: number;
+  perPage?: number;
+}
